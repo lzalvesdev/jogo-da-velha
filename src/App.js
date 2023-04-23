a145a022b2f5df4e5ffd90c4fdb31edcd3b09354
Luiz Henrique <luizha879@gmail.com>
2023-04-23 16:45:42 -0300

    Make the about link

--- src/App.js
+++ src/App.js
@@ -3,18 +3,12 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <div>
+          <a href="#to-do" className="about">
+            Sobre
+          </a>
+        </div>
       </header>
     </div>
   );
