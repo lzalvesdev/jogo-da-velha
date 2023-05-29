8ec81b6fb626f570f1b4d7a7e63fc6a9c0aca13c
Luiz Henrique <luizha879@gmail.com>
2023-05-29 20:13:41 -0300

    Create the object players

--- src/objects/Player/index.js
+++ src/objects/Player/index.js
@@ -1,7 +1,20 @@
 import React from "react";
+import "./styles.css";
+import playerX from "../../img/x.png";
+import playerO from "../../img/circle.png";
 
-const Player = () => {
-  return <button className="player">X</button>;
+const Player = ({ player }) => {
+  const players = [];
+  players["x"] = playerX;
+  players["o"] = playerO;
+
+  return (
+    <>
+      <button className="player">
+        <img src={players[player]} alt={`Jogador ${player.toUpperCase()}`}></img>
+      </button>
+    </>
+  );
 };
 
 export default Player;
